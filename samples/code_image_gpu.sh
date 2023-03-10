6b674a558460cce2f4286dbce55d889dce17aa89
Théo Ladune <theo.ladune@orange.com>
2023-03-10 11:46:31 +0100

    Update results on sample/biville.png and fix error on rm

--- samples/code_image_gpu.sh
+++ samples/code_image_gpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python3 src/encode.py                               \
     --device=cuda:0                                 \
-    -i samples/biville.png                          \
-    -o samples/bitstream.bin                        \
+    --input=samples/biville.png                     \
+    --output=samples/bitstream.bin                  \
     --decoded_img_path=samples/decoded.png          \
     --model_save_path=samples/model.pt              \
     --enc_results_path=samples/encoder_results.txt  \
-    --lmbda=0.001                                   \
+    --lmbda=0.0002                                  \
     --start_lr=1e-2                                 \
     --n_itr=1000                                    \
     --layers_synthesis=12,12                        \
